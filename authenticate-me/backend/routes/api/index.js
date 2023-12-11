cacb535032e7d8aaa0cee9c516d02301c384aa97
Matt Scott <mattscott251@gmail.com>
2023-12-11 12:32:04 -0600

    login route completed

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,18 +1,15 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-// backend/routes/api/index.js
-// ...
+router.use('/session', sessionRouter);
 
-// 
+router.use('/users', usersRouter);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
-  
-// ...
-
-
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
 
